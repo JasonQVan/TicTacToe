38fc3185a7a24b43fb8a28eb6a697f2ce09bcd87
Jason Van <vanquocjason@gmail.com>
2021-06-28 15:32:22 -0400

    tic tac toe

---
+++ js/Game.js
@@ -0,0 +1,69 @@
+export default class Game
+{
+    constructor(){
+        console.log("init game");
+        this.turn = "X";
+        this.board = new Array(9).fill(null);
+    }
+
+    nextTurn(){
+        if(this.turn == "X"){
+            this.turn = "O";
+        }
+        else{
+            this.turn = "X";
+        }
+    }
+
+    makeMove(i){
+        if(this.endOfGame()){
+            return;
+        }
+        if(this.board[i])
+        {
+            return;
+        }
+        this.board[i] = this.turn;
+        let WinningCominbations = this.findWinningCominbations();
+        console.log("This is the winner:", WinningCominbations)
+        if(!WinningCominbations){
+            this.nextTurn()
+        }
+    }
+
+    findWinningCominbations(){
+        const WinningCominbations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2],
+        ]
+
+        for(const combination of WinningCominbations){
+            const [a,b,c] = combination;
+
+            console.log(this.board);
+            if (this.board[a] && (this.board[a] == this.board[b] && this.board[a] == this.board[c])) {
+                return combination
+            
+            }
+        }
+        return null;
+
+    }
+
+    endOfGame(){
+        let WinningCominbations = this.findWinningCominbations();
+        if(WinningCominbations){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
